88473ff95b6445dd75365f666283c1a7bb0e4671
Nassir <na2165@nyu.edu>
2019-06-04 11:46:04 -0400

    Update test_basics.cpp

--- tests/test_basics.cpp
+++ tests/test_basics.cpp
@@ -24,8 +24,7 @@ int main() {
     cout << "j = " << j << endl;
     inc_pass_ref(j);
     cout << "j = " << j << endl;
-    // Put this assertion back once you've written code:
-    // assert(j == 9);
+    assert(j == 9);
     
     int test_num = 10;
     int ret = square(test_num);
