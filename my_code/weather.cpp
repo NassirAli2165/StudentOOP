9f894aa1ae75a5cad33bbb06a623913082667028
Nassir <na2165@nyu.edu>
2019-06-06 11:32:02 -0400

    Finished

--- my_code/weather.cpp
+++ my_code/weather.cpp
@@ -21,11 +21,17 @@ station_nm(nm), my_loc(loc) {
 
 ostream& operator<< (std::ostream& os, const Weather& w){
     os << "Name: " << w.station_nm << endl;
-    os << "Latitude: " << w.my_loc.latitude << ", Longitude: " << w.my_loc.longitude << endl;
+    os << "Latitude: " << w.my_loc.latitude << ", Longitude: " << w.my_loc.longitude << ", Rating: " << w.rating << endl;
     return os;
 }
+int Weather::get_rating() const {
+    return rating;
+}
 
+void Weather::set_rating(int new_rating){
+    rating = new_rating;
+}
 
-string Weather::get_name() {
+string Weather::get_name() const {
     return station_nm;
 }
