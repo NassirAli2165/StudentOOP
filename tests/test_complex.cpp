6047e09efe93bf8bfeb989038eba84cb71b53f67
Nassir <na2165@nyu.edu>
2019-06-06 11:46:25 -0400

    Update test_complex.cpp

--- tests/test_complex.cpp
+++ tests/test_complex.cpp
@@ -20,7 +20,7 @@ int main() {
     cout << "Input a complex number:\n";
     Complex c1;
     // get a complex from stdin:
-    c1 = 12;
+    cin >> c1;
     if(!cin)
     {
         cerr << "Bad input format\n";
