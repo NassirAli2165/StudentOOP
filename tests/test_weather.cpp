8c24957540545fdb5a78606cb2deeee9d51738e1
Nassir <na2165@nyu.edu>
2019-06-07 12:26:33 -0400

    Update test_weather.cpp

--- tests/test_weather.cpp
+++ tests/test_weather.cpp
@@ -32,9 +32,9 @@ void get_wreadings(string filenm, Weather& w) {
 int main() {
     Weather irkutsk = Weather("Irkutsk", GPS(46.3, 67.2));
     
-    string filenm = "r.txt";
-    cout << "Input the name of the new readings file: r.txt";
-    
+    string filenm;
+    cout << "Input the name of the new readings file: ";
+    cin >> filenm;
     
     get_wreadings(filenm, irkutsk);
     
